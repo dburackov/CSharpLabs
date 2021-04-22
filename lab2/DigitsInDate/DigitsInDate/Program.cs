7295a1b92099348a45d05dc582b6a0a32e08be0d
dburackov <79422427+dburackov@users.noreply.github.com>
2021-04-22 20:49:08 +0300

    Add files via upload

---
+++ lab2/DigitsInDate/DigitsInDate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DigitsInDate {
+    class Program {
+        static void Main(string[] args) {
+            string firstFormat = DateTime.Now.ToString("f");
+            string secondFormat = DateTime.Now.ToString("g");
+            Console.WriteLine(firstFormat);
+            Console.WriteLine(secondFormat);
+            int[] result = new int[10];
+            for (int i = 0; i < secondFormat.Length; i++) {
+                int current = (int)secondFormat[i] - 48;
+                if (0 <= current && current <= 9) {
+                    ++result[current];
+                }
+            }
+            for (int i = 0; i < 10; ++i) {
+                Console.WriteLine();
+                Console.Write(i + " |");
+                for (int j = 0; j < result[i]; ++j) {
+                    if (i % 2 == 0) {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                    } else {
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    }
+                    Console.Write("  ");
+                    Console.ResetColor();
+                }
+                if (result[i] > 0) {
+                    Console.Write(" " + result[i]);
+                }
+            }
+        }
+    }
+}
