fb00c6ec65db89b192fee9b4b2e4f5f5d47f6bdd
dburackov <79422427+dburackov@users.noreply.github.com>
2021-05-13 23:54:14 +0300

    Add files via upload

---
+++ lab4/DesktopDrawing/DesktopDrawing/DesktopDrawing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace DesktopDrawing {
+    class DesktopDrawing {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr ptr);
+
+        [DllImport("user32.dll")]
+        static extern void ReleaseDC(IntPtr ptr);
+
+        private static Color color = Color.White;
+
+        public static Color Clr {
+            get {
+                return color;
+            }
+            set {
+                color = value;
+            }
+        }
+
+        public static void Draw(Point a, Point b) {
+            IntPtr DesktopDC = GetDC(IntPtr.Zero);
+            Graphics graphics = Graphics.FromHdc(DesktopDC);
+            Pen pen = new Pen(color);
+            graphics.DrawLine(pen, a, b);
+            graphics.Dispose();
+            ReleaseDC(DesktopDC);
+        }
+    }
+}
---
+++ lab4/DesktopDrawing/DesktopDrawing/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DesktopDrawing {
+    class Program {
+        static void Main(string[] args) {
+            Console.CursorVisible = false;
+            Point previous = new Point();
+            bool initialized = false;
+            List<Color> colorList = new List<Color>() {Color.Blue, Color.Green, Color.Pink, Color.White};
+            int colorIndex = 0;
+            while (true) {
+                if (Console.KeyAvailable) {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Escape) {
+                        break;
+                    } else if (key.Key == ConsoleKey.Spacebar) {
+                        colorIndex = (colorIndex + 1) % colorList.Count;
+                        DesktopDrawing.Clr = colorList[colorIndex];
+                    }
+                } else {
+                    if (initialized) {
+                        Point current = new Point(Cursor.Position.X, Cursor.Position.Y);
+                        DesktopDrawing.Draw(previous, current);
+                        previous = current;
+                    } else {
+                        previous.X = Cursor.Position.X;
+                        previous.Y = Cursor.Position.Y;
+                        initialized = true;
+                    }
+                }
+            }
+        }
+    }
+}
