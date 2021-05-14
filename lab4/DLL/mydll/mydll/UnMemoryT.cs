e72e70db9e8ac778c91102ed3d5b9ff8eb982816
dburackov <79422427+dburackov@users.noreply.github.com>
2021-05-14 12:44:33 +0300

    Add files via upload

---
+++ lab4/DLL/mydll/mydll/UnMemoryT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace mydll {
+    public static class UnMemory<T> where T : struct {
+        public static void SaveInMemory(T memoryObject, ref IntPtr ptr) {
+            if (default(T).Equals(memoryObject)) {
+                ptr = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(T)));
+                UnMemory.Enqueue(ptr);
+            } else {
+                if (ptr == IntPtr.Zero) {
+                    ptr = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(T)));
+                    Marshal.StructureToPtr(memoryObject, ptr, false);
+                } else {
+                    Marshal.StructureToPtr(memoryObject, ptr, true);
+                }
+                UnMemory.Enqueue(ptr);
+            }
+        }
+
+        public static T ReadInMemory(IntPtr ptr) {
+            return (T)Marshal.PtrToStructure(ptr, typeof(T));
+        }
+    }
+}
