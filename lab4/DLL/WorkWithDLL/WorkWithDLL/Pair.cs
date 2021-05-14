ee8d115dbd8fbb3c0404f077e9c639dfd41d854d
dburackov <79422427+dburackov@users.noreply.github.com>
2021-05-14 12:45:46 +0300

    Add files via upload

---
+++ lab4/DLL/WorkWithDLL/WorkWithDLL/Pair.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkWithDLL {
+    class Pair<T> where T : struct {
+        public T value;
+        public IntPtr ptr;
+        public Pair(T value) {
+            this.value = value;
+            ptr = IntPtr.Zero;
+        }
+    }
+}
---
+++ lab4/DLL/WorkWithDLL/WorkWithDLL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using mydll;
+
+namespace WorkWithDLL {
+    class Program {
+        static void Main(string[] args) {
+            Pair<char> a = new Pair<char>('A'); 
+            Pair<char> b = new Pair<char>('B'); 
+            Pair<char> c = new Pair<char>('C');
+
+            Pair<int> i1 = new Pair<int>(12);
+            Pair<int> i2 = new Pair<int>(567);
+            Pair<int> i3 = new Pair<int>(972);
+
+            Pair<bool> x = new Pair<bool>(true);
+            Pair<bool> y = new Pair<bool>(false);
+
+            UnMemory<char>.SaveInMemory(a.value, ref a.ptr);
+            UnMemory<char>.SaveInMemory(b.value, ref b.ptr);
+            UnMemory<char>.SaveInMemory(c.value, ref c.ptr);
+
+            UnMemory<int>.SaveInMemory(i1.value, ref i1.ptr);
+            UnMemory<int>.SaveInMemory(i2.value, ref i2.ptr);
+            UnMemory<int>.SaveInMemory(i3.value, ref i3.ptr);
+
+            UnMemory<bool>.SaveInMemory(x.value, ref x.ptr);
+            UnMemory<bool>.SaveInMemory(y.value, ref y.ptr);
+
+            Console.WriteLine(UnMemory<char>.ReadInMemory(a.ptr));
+            Console.WriteLine(UnMemory<char>.ReadInMemory(b.ptr));
+            Console.WriteLine(UnMemory<char>.ReadInMemory(c.ptr));
+
+            Console.WriteLine(UnMemory<int>.ReadInMemory(i1.ptr));
+            Console.WriteLine(UnMemory<int>.ReadInMemory(i2.ptr));
+            Console.WriteLine(UnMemory<int>.ReadInMemory(i3.ptr));
+
+            Console.WriteLine(UnMemory<bool>.ReadInMemory(x.ptr));
+            Console.WriteLine(UnMemory<bool>.ReadInMemory(y.ptr));
+
+            UnMemory.FreeMemory();
+        }
+    }
+}
