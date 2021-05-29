d7bf9bef9d939cd25a6141433b7f7faa09921feb
dburackov <79422427+dburackov@users.noreply.github.com>
2021-05-29 17:37:46 +0300

    Add files via upload

---
+++ lab6/Transport/Transport/DeLorean.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Media;
+
+namespace Transport {
+    class DeLorean : Car, IObsoleable {
+        public DeLorean(int cost = 0, int loadLimit = 0) : base(cost, loadLimit) {
+            Name = "DeLorean";
+            Relevant = true;
+        }
+        private int timeLeft = 360;
+        public bool Relevant { get; private set; }
+        public void ReduceRelevantTime(int months) {
+            if (months > 0) {
+                timeLeft -= months;
+            }
+            if (timeLeft <= 0) {
+                Relevant = false;
+            }
+        }
+        public override void Horn() {
+            SoundPlayer horn = new SoundPlayer(@"..\..\src\deloreanhorn.wav");
+            horn.PlaySync();
+        }
+        public override void Move() {
+            SoundPlayer move = new SoundPlayer(@"..\..\src\deloreanmove.wav");
+            move.PlaySync();
+        }
+    }
+}
---
+++ lab6/Transport/Transport/Tesla.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Media;
+
+namespace Transport {
+    class Tesla : Car {
+        public Tesla(int cost = 0, int loadLimit = 0) : base(cost, loadLimit) {
+            Name = "Tesla";
+            Condition = "Full battery";
+        }
+        public int batteryLevel = 100;
+        public string Condition { get; set; }
+        public void Charging () {
+            batteryLevel = 100;
+            Condition = "Full baterry";
+        }
+        public override void Horn() {
+            SoundPlayer horn = new SoundPlayer(@"..\..\src\teslahorn.wav");
+            horn.PlaySync();
+        }
+        public override void Move() {
+            SoundPlayer move = new SoundPlayer(@"..\..\src\teslamove.wav");
+            move.PlaySync();
+            batteryLevel -= 10;
+            if (batteryLevel <= 0) {
+                Condition = "Empty battery";
+            } else if (batteryLevel <= 30) {
+                Condition = "Low battery";
+            }
+        }
+    }
+}
